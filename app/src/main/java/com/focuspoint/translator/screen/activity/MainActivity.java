66182744f317ebda77eec55afa593054c2a19134
FocusPoint <newvovkka@gmail.com>
2017-04-19 23:32:24 +0300

    handle back button

--- app/src/main/java/com/focuspoint/translator/screen/activity/MainActivity.java
+++ app/src/main/java/com/focuspoint/translator/screen/activity/MainActivity.java
@@ -1,44 +1,27 @@
 package com.focuspoint.translator.screen.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+
 
 import com.focuspoint.translator.App;
 import com.focuspoint.translator.R;
 import com.focuspoint.translator.adapters.TranslatePagerAdapter;
-import com.focuspoint.translator.models.Language;
 import com.focuspoint.translator.screen.Navigator;
-import com.focuspoint.translator.screen.TranslationScreenContract;
-import com.focuspoint.translator.screen.fragment.FavoritesFragment;
-import com.focuspoint.translator.screen.fragment.HistoryFragment;
-import com.focuspoint.translator.screen.fragment.TranslateFragment;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
-import retrofit2.Retrofit;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    @Inject Retrofit retrofit;
-
-
 
     @BindView(R.id.view_pager) ViewPager viewPager;
     @BindView(R.id.tab_layout) TabLayout tabLayout;
@@ -50,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         initViews();
     }
 
@@ -77,15 +59,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    /**Move to screen using Navigator enum*/
     public void goToFragment(Navigator screen){
         if (screen == Navigator.SCREEN_TRANSLATION){
             viewPager.setCurrentItem(0, true);
         }
     }
 
-
+    /**Obtain MainActivity instance from Fragment*/
     public static MainActivity from(Fragment fragment){
         return (MainActivity) fragment.getActivity();
+    }
+
+    //return to main screen if '<' pressed;
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() != 0){
+            viewPager.setCurrentItem(0);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
